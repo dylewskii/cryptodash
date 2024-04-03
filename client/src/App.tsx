9fb0c805b73100bf7ea27e14ae08e77e31f6a3ce
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-03 16:32:55 +0100

    add MainContent component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import MainContent from "./components/MainContent.tsx";
 import Sidebar from "./components/Sidebar.tsx";
 
 function App() {
   return (
     <div className="flex flex-row min-h-screen">
       <Sidebar />
+      <MainContent>
+        <div>content</div>
+      </MainContent>
     </div>
   );
 }
--- client/src/components/MainContent.tsx
+++ client/src/components/MainContent.tsx
@@ -1,3 +1,9 @@
-export default function MainContent() {
-  return <main></main>;
+import React from "react";
+
+type MainContentProps = {
+  children: React.ReactNode;
+};
+
+export default function MainContent({ children }: MainContentProps) {
+  return <main className="flex-1">{children}</main>;
 }
