6d7944b6e445436a922ea9c2ec003548f0be7575
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-04 11:24:08 +0100

    import TopCoins

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -1,65 +1,11 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import TopCoins from "./TopCoins";
 import TotalBalance from "./TotalBalance";
 
 export default function Dashboard() {
   return (
     <section>
       <TotalBalance />
-      <div className="custom-grid-1 mx-1 my-2 p-4">
-        <Card>
-          <CardHeader>
-            <CardTitle>Bitcoin</CardTitle>
-            <CardDescription>BTC</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p className="text-3xl">
-              <span>$</span>80,000
-            </p>
-          </CardContent>
-        </Card>
-
-        <Card>
-          <CardHeader>
-            <CardTitle>Ethereum</CardTitle>
-            <CardDescription>ETH</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p className="text-3xl">
-              <span>$</span>5000
-            </p>
-          </CardContent>
-        </Card>
-
-        <Card>
-          <CardHeader>
-            <CardTitle>Solana</CardTitle>
-            <CardDescription>SOL</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p className="text-3xl">
-              <span>$</span>300
-            </p>
-          </CardContent>
-        </Card>
-
-        <Card>
-          <CardHeader>
-            <CardTitle>Dogecoin</CardTitle>
-            <CardDescription>DOGE</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p className="text-3xl">
-              <span>$</span>1
-            </p>
-          </CardContent>
-        </Card>
-      </div>
+      <TopCoins />
     </section>
   );
 }
