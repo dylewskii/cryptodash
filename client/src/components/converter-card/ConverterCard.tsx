27ea45fb78835cc97d36a952d76822935a3b8f95
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-04 15:48:32 +0100

    add ConverterCard

---
+++ client/src/components/converter-card/ConverterCard.tsx
@@ -0,0 +1,25 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function ConverterCard() {
+  return (
+    <div className="flex flex-col">
+      <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Conversion Checker</CardTitle>
+            <CardDescription></CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className=""></p>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
