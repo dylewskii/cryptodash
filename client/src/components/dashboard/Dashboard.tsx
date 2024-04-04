d0351308b18e4c806f05fcb47f975a9446087595
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-04 11:22:23 +0100

    import TotalBalance

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -5,35 +5,12 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import TotalBalance from "./TotalBalance";
 
 export default function Dashboard() {
   return (
     <section>
-      <div className="flex flex-col mx-2 my-4">
-        Total Balance
-        <div className="flex gap-3">
-          <p className="text-5xl">
-            <span>$</span>100,000
-          </p>
-          <p className="flex items-center text-green-500">
-            <svg
-              className="w-4 h-4"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M18 15L12 9L6 15"
-                stroke="rgb(34 197 94)"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            10%
-          </p>
-        </div>
-      </div>
+      <TotalBalance />
       <div className="custom-grid-1 mx-1 my-2 p-4">
         <Card>
           <CardHeader>
