036cc2dcb32655d0e6e85d2adb78085e67c950cd
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-04 12:29:02 +0100

    change mainContent to grid container

--- client/src/components/main-content/MainContent.tsx
+++ client/src/components/main-content/MainContent.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./mainContent.scss";
 
 type MainContentProps = {
   children: React.ReactNode;
 };
 
 export default function MainContent({ children }: MainContentProps) {
-  return <main className="flex-1">{children}</main>;
+  return <main className="flex-1 mainContent">{children}</main>;
 }
