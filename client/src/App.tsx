0e18f661ee18b45adde59e41adbb45f4060dbe79
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-06 17:33:28 +0100

    update dir name

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Dashboard from "./components/dashboard/Dashboard.tsx";
+import Dashboard from "./components/dashboard-panel/Dashboard.tsx";
 import MainContent from "./components/main-content/MainContent.tsx";
 import Sidebar from "./components/sidebar/Sidebar.tsx";
 
