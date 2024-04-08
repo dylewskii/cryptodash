9e8fb10923484b9bb905115836a2d310db098ef2
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-08 10:35:54 +0100

    add Fear/Greed Index Card

---
+++ client/src/components/dashboard-panel/FGCard.tsx
@@ -0,0 +1,35 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+// Fear / Greed Index Score
+export default function FGCard() {
+  return (
+    <div className="md:m-2 md:px-2">
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex items-center gap-2">
+            Fear & Greed Index
+            <a>
+              <svg
+                className="h-4 w-4"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M12 16V12M12 8H12.01M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </a>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>SCORE</p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
