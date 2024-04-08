cd33f6248e104089741df93374f8a81234395879
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-08 18:58:47 +0100

    add max width

--- client/src/components/main-content/MainContent.tsx
+++ client/src/components/main-content/MainContent.tsx
@@ -7,7 +7,7 @@ type MainContentProps = {
 
 export default function MainContent({ children }: MainContentProps) {
   return (
-    <main className="flex-1 px-8 mt-4 md:grid md:grid-cols-5 md:grid-rows-[auto_auto_auto] md:px-0 md:pr-8">
+    <main className="flex-1 px-8 mt-4 md:grid md:grid-cols-5 md:grid-rows-[auto_auto_auto] md:px-0 md:pr-8 max-w-screen-2xl">
       {children}
     </main>
   );
