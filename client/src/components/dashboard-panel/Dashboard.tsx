4db1c9bedba00cb730fd5c70cec39a2a0fbd4958
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-08 18:49:25 +0100

    fix Dashboard layout

--- client/src/components/dashboard-panel/Dashboard.tsx
+++ client/src/components/dashboard-panel/Dashboard.tsx
@@ -2,17 +2,25 @@ import ConverterCard from "./ConverterCard";
 import PortfolioCard from "./PortfolioCard";
 import FavoriteCoins from "./FavoriteCoins";
 import TotalBalance from "./TotalBalance";
+import FGCard from "./FGCard";
+import TotalMcapCard from "./TotalMcapCard";
 
 export default function Dashboard() {
   return (
     <>
-      <section className="col-span-3">
+      <section className="col-span-3 row-start-1">
         <TotalBalance />
         <PortfolioCard />
+      </section>
+      <section className="col-span-5 row-start-2">
         <FavoriteCoins />
       </section>
-      <section className="grid-col-4 col-span-2">
-        <ConverterCard />
+      <ConverterCard
+        className={"col-start-4 col-span-2 row-start-1 md:mt-24 md:pl-4"}
+      />
+      <section className="col-span-5 row-start-3 mt-4 flex gap-4">
+        <FGCard className={""} />
+        <TotalMcapCard />
       </section>
     </>
   );
--- client/src/components/main-content/MainContent.tsx
+++ client/src/components/main-content/MainContent.tsx
@@ -7,7 +7,7 @@ type MainContentProps = {
 
 export default function MainContent({ children }: MainContentProps) {
   return (
-    <main className="flex-1 mt-4 px-8 md:grid md:grid-cols-5 md:px-14">
+    <main className="flex-1 px-8 mt-4 md:grid md:grid-cols-5 md:grid-rows-[auto_auto_auto] md:px-0 md:pr-8">
       {children}
     </main>
   );
