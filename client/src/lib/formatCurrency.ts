99747406465db32b0c7d319ea95e532fc1da62e0
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-10 12:28:21 +0100

    add formatCurrency

---
+++ client/src/lib/formatCurrency.ts
@@ -0,0 +1,12 @@
+const currencyFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+const formatCurrency = (value: number) => {
+  return currencyFormatter.format(value);
+};
+
+export default formatCurrency;
