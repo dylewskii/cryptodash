d7c15c0aaf799ed74fe2c39324997a8060cc7d2b
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-10 10:45:25 +0100

    fix Button padding and colors

--- client/src/components/send-panel/AssetTransferOptions.tsx
+++ client/src/components/send-panel/AssetTransferOptions.tsx
@@ -3,10 +3,10 @@ import { Button } from "@/components/ui/button";
 export default function AssetTransferOptions() {
   return (
     <div className="flex justify-center gap-4">
-      <Button className="bg-white text-black px-10 w-34 rounded-xl">
+      <Button className="bg-orange-950 text-white px-10 py-6 w-44 rounded-xl hover:bg-orange-900">
         Send
       </Button>
-      <Button className="bg-zinc-400 text-white px-10 w-34 rounded-xl">
+      <Button className="bg-orange-500 text-white px-10 py-6 w-44 rounded-xl hover:bg-orange-300">
         Receive
       </Button>
     </div>
