4e9a661d1db52bb01ab6a42530e56a41723bd76b
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-13 11:18:42 +0100

    connect mongoose

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(() => console.log("MongoDB connection error:", err));
 
 app.use(express.json());
 
 app.use("/api", (req, res, next) => {
-  res.json({ users: ["user1", "user2", "user3"] });
+  res.json({ coins: ["coin1", "coin2", "coin3"] });
 });
 
 app.listen(PORT, () => {
