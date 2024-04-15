28fdb2cb025dd9f6c77bb04cc20448e99f1fc977
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-15 18:27:40 +0100

    add login/register routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,35 @@
+// react
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// pages
+import LoginPage from "./components/login-page/LoginPage.tsx";
+import RegisterPage from "./components/register-page/RegisterPage.tsx";
+// components
 import MainLayout from "./components/layout/MainLayout.tsx";
 import Dashboard from "./components/dashboard-panel/Dashboard.tsx";
 import AssetsPanel from "./components/assets-panel/AssetsPanel.tsx";
 import ExplorePanel from "./components/explore-panel/ExplorePanel.tsx";
 import SwapPanel from "./components/swap-panel/SwapPanel.tsx";
 import SendPanel from "./components/send-panel/SendPanel.tsx";
+// css
 import "./globals.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <LoginPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/app",
     element: <MainLayout />,
     children: [
       { path: "home", element: <Dashboard /> },
