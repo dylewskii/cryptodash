4894e2522a307bacf00b1f9eae43e0ec9fbe9b57
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-15 18:01:32 +0100

    add LoginPage

---
+++ client/src/components/login-page/LoginPage.tsx
@@ -0,0 +1,59 @@
+import { SyntheticEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    // verify the user details
+    if (username && password) {
+      navigate("/app/home");
+    } else {
+      alert("Username or password incorrect.");
+    }
+  };
+
+  return (
+    <section className="h-full flex justify-center items-center">
+      <div>
+        <h2 className="text-2xl text-center mb-4">Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="username">Username:</label>
+            <Input
+              id="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="password">Password:</label>
+            <Input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-4">
+            <p>
+              Don't have an account? <a href="/register">Sign Up</a>
+            </p>
+          </div>
+
+          <Button type="submit" className="w-80">
+            Login
+          </Button>
+        </form>
+      </div>
+    </section>
+  );
+}
