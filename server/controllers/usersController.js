18114146fff099b4c95c322de00a52c5a9bf40b0
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-17 13:04:53 +0100

    set up usersController

---
+++ server/controllers/usersController.js
@@ -0,0 +1,46 @@
+const bcryptjs = require("bcryptjs");
+const User = require("../models/User");
+
+const registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ msg: "User already exists." });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    return res.json({ msg: "User Registered Succesfully" });
+  } catch (error) {
+    console.error("Server error:", error);
+    return res.status(500).json({ msg: "Internal server error" });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.json({ msg: "User not found." });
+    }
+
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return res.json({ msg: "Incorrect Credentials" });
+    }
+  } catch (error) {}
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,45 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const bcryptjs = require("bcryptjs");
-const User = require("../models/User");
+const usersController = require("../controllers/usersController");
 
-router.post("/register", async (req, res) => {
-  const { username, email, password } = req.body;
+router.post("/register", usersController.registerUser);
 
-  try {
-    const user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({ msg: "User already exists." });
-    }
-
-    const hashedPassword = await bcryptjs.hash(password, 10);
-    const newUser = new User({
-      username,
-      email,
-      password: hashedPassword,
-    });
-
-    await newUser.save();
-    return res.json({ msg: "User Registered Succesfully" });
-  } catch (error) {
-    console.error("Server error:", error);
-    return res.status(500).json({ msg: "Internal server error" });
-  }
-});
-
-router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const user = await User.findOne({ username });
-    if (!user) {
-      return res.json({ msg: "User not found." });
-    }
-
-    const validPassword = await bcryptjs.compare(password, user.password);
-    if (!validPassword) {
-      return res.json({ msg: "Incorrect Credentials" });
-    }
-  } catch (error) {}
-});
+router.post("/login", usersController.loginUser);
 
 module.exports = router;
