d8010603d6ddc1322d8978333af74993b62d87f5
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-19 17:09:03 +0100

    fetch portfolio coin data

--- client/src/components/dashboard-panel/PortfolioCard.tsx
+++ client/src/components/dashboard-panel/PortfolioCard.tsx
@@ -11,21 +11,82 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { useEffect, useState } from "react";
+
+interface Coin {
+  name: string;
+  symbol: string;
+  image: string;
+  currentPrice: number;
+}
 
 export default function PortfolioCard() {
-  const portfolioCoins: string[] = [
-    "Bitcoin",
-    "Ethereum",
-    "Solana",
-    "Dogecoin",
-  ];
+  const portfolioCoins = ["bitcoin", "ethereum", "solana", "dogecoin"];
+  const [portfolio, setPortfolio] = useState<Coin[]>([]);
+  const [addedCoin, setAddedCoin] = useState<string>("");
+  const [isDialogOpen, setDialogOpen] = useState(false);
+
+  useEffect(() => {
+    fetchPortfolioCoinData(portfolioCoins);
+  }, []);
+
+  const fetchPortfolioCoinData = async (coins: string[]) => {
+    const requests = coins.map((coin) => {
+      const url = `https://api.coingecko.com/api/v3/coins/${coin}`;
+      return fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          return {
+            name: data.name,
+            symbol: data.symbol,
+            image: data.image.small,
+            currentPrice: data.market_data.current_price.usd,
+          };
+        })
+        .catch((error) => {
+          console.error("Error fetching portfolio data:", error);
+          return null;
+        });
+    });
+
+    Promise.all(requests)
+      .then((results) => {
+        const filteredResults = results.filter(
+          (result) => result !== null
+        ) as Coin[]; // filter out any nulls & assert type Coin[]
+        setPortfolio(filteredResults);
+        console.log(results);
+      })
+      .catch((error) => console.error("Error with Promise.all:", error));
+  };
+
+  const addCoinToPortfolio = async () => {
+    if (!addedCoin) return;
+
+    const url = `https://api.coingecko.com/api/v3/coins/${addedCoin.toLowerCase()}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const newCoin = {
+        name: data.name,
+        symbol: data.symbol,
+        image: data.image.thumb,
+        currentPrice: data.market_data.current_price.usd,
+      };
+      setPortfolio((prevState) => [...prevState, newCoin]);
+      setDialogOpen(false);
+      console.log(`${newCoin.name} has been added`);
+    } catch (error) {
+      console.error("Error fetching data for new coin:", error);
+    }
+  };
 
   return (
     <ScrollArea className="rounded-md border">
       <div className="p-4">
         <div className="flex justify-between mb-6">
           <h4 className="text-lg font-medium leading-none">Portfolio</h4>
-          <Dialog>
+          <Dialog open={isDialogOpen} onOpenChange={setDialogOpen}>
             <DialogTrigger>
               <svg
                 className="w-6 h-6"
@@ -56,7 +117,9 @@ export default function PortfolioCard() {
                   </label>
                   <Input
                     id="coinName"
-                    defaultValue="Bitcoin"
+                    placeholder="Bitcoin"
+                    value={addedCoin}
+                    onChange={(e) => setAddedCoin(e.target.value)}
                     className="col-span-3"
                   />
                 </div>
@@ -72,25 +135,30 @@ export default function PortfolioCard() {
                 </div>
               </div>
               <DialogFooter>
-                <Button type="submit">Save</Button>
+                <Button type="submit" onClick={addCoinToPortfolio}>
+                  Save
+                </Button>
               </DialogFooter>
             </DialogContent>
           </Dialog>
         </div>
 
-        {portfolioCoins.map((coin, i) => (
+        {portfolio.map((coin, i) => (
           <div key={i} className="grid grid-cols-[50px_3fr] gap-2">
             <span className="grid place-items-center grid-col-1 max-w-6">
-              <img src="./" />
+              <img src={coin?.image} alt={coin?.name} />
             </span>
             <div className="grid-col-2 flex justify-between">
               <div className="flex flex-col">
-                <p>{coin}</p>
-                <p className="text-zinc-500">$550</p>
+                <p>{coin.name}</p>
+                <p className="text-zinc-500">
+                  <span>$</span>
+                  {coin.currentPrice}
+                </p>
               </div>
               <div className="flex items-center">
                 <p className="text-sm">
-                  1.72 <span>BTC</span>
+                  1.72 <span>{coin.symbol.toUpperCase()}</span>
                 </p>
               </div>
             </div>
