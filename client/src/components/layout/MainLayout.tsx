2da41f6c4176bfba0d9ba5e7fc4c07d48d7f57d6
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-24 11:32:15 +0100

    add Toaster

--- client/src/components/layout/MainLayout.tsx
+++ client/src/components/layout/MainLayout.tsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../navbar/Navbar";
 import Sidebar from "../sidebar/Sidebar";
 import MainContent from "../main-content/MainContent";
+import { Toaster } from "../ui/toaster";
 
 export default function MainLayout() {
   return (
     <>
       <Navbar />
+      <Toaster />
       <div className="flex flex-col md:flex-row">
         <Sidebar />
         <MainContent>
