5f905b5878df3bdc72063afd0c6c47b28630dc38
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-04-24 09:51:43 +0100

    improve error handling and change fetch endpoint

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -38,14 +38,23 @@ export function UserProvider({ children }: UserProviderProps) {
 
   useEffect(() => {
     if (!user.userId) {
-      fetch("/profile")
-        .then((res) => res.json())
+      fetch("http://localhost:8000/profile", {
+        credentials: "include",
+      })
+        .then((res) => {
+          if (!res.ok) throw new Error("Failed to fetch profile");
+          return res.json();
+        })
         .then((data) => {
           console.log("Fetched user data:", data);
           setUser(data);
+        })
+        .catch((error) => {
+          console.error("Error fetching user data:", error);
+          setUser({ userId: "", username: "" });
         });
     }
-  }, [user]);
+  }, []);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
