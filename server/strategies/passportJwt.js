03713250cfa3ff020c478dbc8a19e41ba194d108
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-05-01 23:19:07 +0200

    add passport jwt strategy

---
+++ server/strategies/passportJwt.js
@@ -0,0 +1,31 @@
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+require("dotenv").config();
+
+const opts = {};
+
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_KEY,
+};
+
+passport.use(
+  new JwtStrategy(options, async (jwt_payload, done) => {
+    try {
+      const user = await User.findById(jwt_payload.id);
+
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: "User not found" });
+      }
+    } catch (error) {
+      return done(error, false);
+    }
+  })
+);
+
+module.exports = passport;
