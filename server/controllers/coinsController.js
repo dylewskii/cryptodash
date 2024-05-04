9f809dcefecdf3e7e9ab76d1e19f39df0853aa0e
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-05-04 15:59:58 +0200

    begin working on addCoin controller

--- server/controllers/coinsController.js
+++ server/controllers/coinsController.js
@@ -7,4 +7,20 @@ const getAllCoins = async (req, res) => {
   }
 };
 
-module.exports = getAllCoins;
+const addCoin = (req, res) => {
+  const { name, amount } = req.body;
+
+  if (name.length < 2) {
+    res.json({ sucess: false, msg: "Coin name invalid" });
+  }
+
+  res.json({
+    message: "Coin data received successfully",
+    receivedData: req.body,
+  });
+};
+
+module.exports = {
+  getAllCoins,
+  addCoin,
+};
