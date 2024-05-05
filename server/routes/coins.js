7b244223f58ac0097c02c952a9d204cb0e9971e6
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-05-05 15:24:40 +0200

    add authentication middleware

--- server/routes/coins.js
+++ server/routes/coins.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const Coin = require("../models/Coin");
+const passport = require("passport");
 const coinsController = require("../controllers/coinsController");
 
 // GET -> all coins
-router.get("/all", coinsController.getAllCoins);
+// router.get("/all", coinsController.getAllCoins);
 
 // POST -> add a coin
-router.post("/add", coinsController.addCoin);
+router.post(
+  "/add",
+  passport.authenticate("jwt", { session: false }),
+  coinsController.addCoin
+);
 
 module.exports = router;
