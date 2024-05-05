b0e5970ea04187cb0d1c26a43fdf25720390c4fd
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-05-05 15:41:40 +0200

    implement addCoin - push newCoin to users portfolio

--- server/controllers/coinsController.js
+++ server/controllers/coinsController.js
@@ -1,26 +1,56 @@
-const getAllCoins = async (req, res) => {
-  try {
-    const coins = await Coin.find();
-    res.json(coins);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
+const User = require("../models/User");
 
-const addCoin = (req, res) => {
+// const getAllCoins = async (req, res) => {
+//   try {
+//     res.json({success: true, msg: "Here is a list of all your coins"});
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// };
+
+const addCoin = async (req, res) => {
   const { name, amount } = req.body;
+  const userId = req.user.id;
 
-  if (name.length < 2) {
+  // check if coin name is valid - i.e not shorter than 1 character
+  if (name.length < 1) {
     res.json({ sucess: false, msg: "Coin name invalid" });
+    return;
   }
 
-  res.json({
-    message: "Coin data received successfully",
-    receivedData: req.body,
-  });
+  // convert amount to float num
+  const numericAmount = parseFloat(amount);
+  if (isNaN(numericAmount)) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Amount must be a number." });
+  }
+
+  try {
+    const user = await User.findById(userId);
+    console.log(user);
+    if (!user) {
+      return res.status(404).json({ success: false, msg: "User not found" });
+    }
+
+    // add coin to user's portfolio array
+    const newCoin = { name, amount: numericAmount };
+    user.portfolio.push(newCoin);
+
+    await user.save();
+
+    res.json({
+      success: true,
+      message: "Coin added to portfolio successfully",
+      coinData: newCoin,
+    });
+  } catch (err) {
+    console.error("Failed to add coin:", err);
+    res.status(500).json({ success: false, message: err.message });
+  }
 };
 
 module.exports = {
-  getAllCoins,
+  // getAllCoins,
   addCoin,
 };
