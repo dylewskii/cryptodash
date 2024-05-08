67a6257ab5bb267a26c52accf1d2059c236424eb
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-05-08 13:22:50 +0200

    move portfolioList fetching logic inside UserContext

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -6,16 +6,31 @@ import {
   Dispatch,
   SetStateAction,
 } from "react";
+import { fetchPortfolioList } from "@/lib/portfolioUtils";
 
+// ------------------------------- TYPES -------------------------------
+// shape of user data
 interface UserType {
   userId: string;
   username: string;
+  isAuthenticated: boolean;
 }
 
+// default user object (used before user authentication is confirmed)
 const defaultUser: UserType = {
   userId: "",
   username: "",
+  isAuthenticated: false,
 };
+
+// shape of each portfolio item
+interface PortfolioType {
+  name: string;
+  amount: number;
+}
+
+// default empty array for portfolio items
+const defaultPortfolio: PortfolioType[] = [];
 
 interface UserProviderProps {
   children: ReactNode;
@@ -24,40 +39,41 @@ interface UserProviderProps {
 interface UserContextType {
   user: UserType;
   setUser: Dispatch<SetStateAction<UserType>>;
+  portfolioList: PortfolioType[];
+  setPortfolioList: Dispatch<SetStateAction<PortfolioType[]>>;
 }
 
 const defaultContextValue: UserContextType = {
   user: defaultUser,
   setUser: () => {},
+  portfolioList: defaultPortfolio,
+  setPortfolioList: () => {},
 };
 
 const UserContext = createContext<UserContextType>(defaultContextValue);
 
+// ---------------------------- UserProvider ----------------------------
 export function UserProvider({ children }: UserProviderProps) {
   const [user, setUser] = useState<UserType>(defaultUser);
+  const [portfolioList, setPortfolioList] =
+    useState<PortfolioType[]>(defaultPortfolio);
 
+  // fetch user's portfolio when userId becomes available
   useEffect(() => {
-    if (!user.userId) {
-      fetch("http://localhost:8000/profile", {
-        credentials: "include",
-      })
-        .then((res) => {
-          if (!res.ok) throw new Error("Failed to fetch profile");
-          return res.json();
+    if (user.userId) {
+      // fetch string array of every coin held by user in DB
+      fetchPortfolioList()
+        .then((portfolioArrayFromDB) => {
+          setPortfolioList(portfolioArrayFromDB);
         })
-        .then((data) => {
-          console.log("Fetched user data:", data);
-          setUser(data);
-        })
-        .catch((error) => {
-          console.error("Error fetching user data:", error);
-          setUser({ userId: "", username: "" });
-        });
+        .catch((error) => console.error("Error fetching portfolio", error));
     }
-  }, []);
+  }, [user.userId]);
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider
+      value={{ user, setUser, portfolioList, setPortfolioList }}
+    >
       {children}
     </UserContext.Provider>
   );
