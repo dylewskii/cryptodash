9935dc92e45239ccd240adf2896f1cef438fc58d
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-05-11 17:00:47 +0200

    set up /coins-list-with-data route

--- server/routes/data.js
+++ server/routes/data.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const passport = require("passport");
 const dataController = require("../controllers/dataController");
 
+// GET -> /coins-list-with-data
+router.get(
+  "/coins-list-with-data",
+  passport.authenticate("jwt", { session: false }),
+  dataController.fetchCoinsListWithMarketData
+);
+
 // GET -> /total-market-cap
 router.get(
   "/total-market-cap",
