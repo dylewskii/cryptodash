7dc06343eafec460040608db125009284b33e115
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-05-11 17:08:00 +0200

    use fetchCryptoCoinsList to render table data

--- client/src/components/explore-panel/CryptoListTable.tsx
+++ client/src/components/explore-panel/CryptoListTable.tsx
@@ -6,51 +6,87 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { useEffect, useState } from "react";
+import formatCurrency from "@/lib/formatCurrency";
+
+interface CoinObject {
+  ath: number;
+  ath_change_percentage: number;
+  ath_date: string;
+  atl: number;
+  atl_change_percentage: number;
+  atl_date: string;
+  circulating_supply: number;
+  current_price: 60907;
+  fully_diluted_valuation: number;
+  high_24h: number;
+  id: string;
+  image: string;
+  last_updated: string;
+  low_24h: number;
+  market_cap: number;
+  market_cap_change_24h: number;
+  market_cap_change_percentage_24h: number;
+  market_cap_rank: number;
+  max_supply: number;
+  name: string;
+  price_change_24h: number;
+  price_change_percentage_24h: number;
+  roi: null;
+  symbol: string;
+  total_supply: number;
+  total_volume: number;
+}
 
 export default function CryptoListTable() {
+  const [cryptoList, setCryptoList] = useState<CoinObject[]>([]);
+
+  useEffect(() => {
+    const fetchCryptoCoinsList = async () => {
+      const url = `http://localhost:8000/data/coins-list-with-data`;
+      try {
+        const response = await fetch(url, {
+          credentials: "include",
+        });
+        const data = await response.json();
+        setCryptoList(data.data);
+      } catch (err) {
+        console.error("Failed to fetch coins list from API:", err);
+      }
+    };
+    fetchCryptoCoinsList();
+  }, []);
+
   return (
     <Table className="mt-4">
       <TableHeader>
         <TableRow>
-          <TableHead className="w-[100px]">Rank</TableHead>
+          <TableHead className="w-[25px]">Rank</TableHead>
+          <TableHead className="w-[25px]">Icon</TableHead>
+
           <TableHead>Name</TableHead>
           <TableHead>Price</TableHead>
           <TableHead className="text-right">Market Cap</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        <TableRow>
-          <TableCell className="font-medium">1</TableCell>
-          <TableCell>Bitcoin</TableCell>
-          <TableCell>
-            <span>$</span>70,000
-          </TableCell>
-          <TableCell className="text-right">
-            <span>$</span>1,360,915,644,546
-          </TableCell>
-        </TableRow>
-
-        <TableRow>
-          <TableCell className="font-medium">2</TableCell>
-          <TableCell>Ethereum</TableCell>
-          <TableCell>
-            <span>$</span>4000
-          </TableCell>
-          <TableCell className="text-right">
-            <span>$</span>424,449,098,451
-          </TableCell>
-        </TableRow>
-
-        <TableRow>
-          <TableCell className="font-medium">3</TableCell>
-          <TableCell>Solana</TableCell>
-          <TableCell>
-            <span>$</span>200
-          </TableCell>
-          <TableCell className="text-right">
-            <span>$</span>$76,299,526,296
-          </TableCell>
-        </TableRow>
+        {cryptoList.map((coinObject) => {
+          return (
+            <TableRow>
+              <TableCell className="font-medium flex items-center">
+                {coinObject.market_cap_rank}
+              </TableCell>
+              <TableCell>
+                <img src={coinObject.image} alt=""></img>
+              </TableCell>
+              <TableCell>{coinObject.name}</TableCell>
+              <TableCell>{formatCurrency(coinObject.current_price)}</TableCell>
+              <TableCell className="text-right">
+                {formatCurrency(coinObject.market_cap)}
+              </TableCell>
+            </TableRow>
+          );
+        })}
       </TableBody>
     </Table>
   );
