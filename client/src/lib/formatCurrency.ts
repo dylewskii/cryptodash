cebd1825a000a215f7d53187ac3cb9930ab39b4f
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-05-12 20:45:57 +0200

    add currency argument

--- client/src/lib/formatCurrency.ts
+++ client/src/lib/formatCurrency.ts
@@ -1,11 +1,11 @@
-const currencyFormatter = new Intl.NumberFormat("en-US", {
-  style: "currency",
-  currency: "USD",
-  minimumFractionDigits: 2,
-  maximumFractionDigits: 2,
-});
+const formatCurrency = (value: number, currency: string = "USD") => {
+  const currencyFormatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: currency,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
 
-const formatCurrency = (value: number) => {
   return currencyFormatter.format(value);
 };
 
