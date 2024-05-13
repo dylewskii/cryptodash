50ccd2962548463cd0cf2ed8192af669407f854f
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-05-13 21:02:47 +0200

    render basic coin data

--- client/src/components/coin-panel/CoinPanel.tsx
+++ client/src/components/coin-panel/CoinPanel.tsx
@@ -1,10 +1,49 @@
-import { useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { Button } from "../ui/button";
+import formatCurrency from "@/lib/formatCurrency";
 
 export default function CoinPanel() {
-  const { coinName } = useParams();
+  const location = useLocation();
+  const coinData = location.state.coin;
+
   return (
     <div>
-      <h1>{coinName} page</h1>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle className="flex gap-2">
+            <img
+              src={coinData.info.image}
+              alt="Cryptocurrency Icon"
+              className="h-16 md:h-24"
+            />
+            <div>
+              <p className="text-2xl">
+                {coinData.name} &#40;{coinData.info.symbol.toUpperCase()}&#41;
+              </p>
+              <p className="flex justify-center text-4xl">
+                {formatCurrency(coinData.info.currentPrice)}
+              </p>
+            </div>
+            <CardDescription></CardDescription>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>Current Holdings: {coinData.amount}</p>
+          <p>Total Value: {formatCurrency(coinData.totalValue)}</p>
+        </CardContent>
+        <CardFooter className="flex justify-end">
+          <Button>Edit Position</Button>
+        </CardFooter>
+      </Card>
+      <div className="flex"></div>
     </div>
   );
 }
