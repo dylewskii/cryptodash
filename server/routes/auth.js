f818309683692a22837fa054d8d90d1624801cf9
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-05-18 16:47:14 +0200

    add reset password & request reset route. Delete getProfile route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
-const passport = require("passport");
 
+// POST -> register new user
 router.post("/register", authController.registerUser);
 
+// POST -> login user
 router.post("/login", authController.loginUser);
 
+// GET -> logout user
 router.get("/logout", authController.logoutUser);
 
-router.get(
-  "/profile",
-  passport.authenticate("jwt", { session: false }),
-  authController.getProfile
-);
+// POST -> reset password
+router.post("/reset-password", authController.resetPassword);
+
+// POST -> request a password reset link
+router.post("/request-password-reset", authController.requestPasswordReset);
 
 module.exports = router;
