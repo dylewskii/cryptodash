3611d39e14b35d2b119cab256fd7ba6529643362
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-05-27 15:15:25 +0200

    fix import

--- client/src/components/ui/mode-toggle.tsx
+++ client/src/components/ui/mode-toggle.tsx
@@ -7,7 +7,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { useTheme } from "../../context/ThemeContext";
+import { useTheme } from "../../hooks/useTheme";
 
 export default function ModeToggle() {
   const { setTheme } = useTheme();
