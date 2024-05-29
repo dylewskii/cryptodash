263804c02ae2e3c4f34950509db5c814ebd7f7e8
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-05-29 10:25:45 +0200

    add validateCoinName

---
+++ server/services/coinServices.js
@@ -0,0 +1,28 @@
+/**
+ * Validates if a coin exists within the CoinGecko API - returns true or false
+ * @param {string} coinName string name of coin to validate
+ * @returns {Promise<boolean>} a promise that resolves to true if coin exists or false otherwise
+ */
+const validateCoinName = async (coinName) => {
+  const url = "https://api.coingecko.com/api/v3/coins/list";
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      "x-cg-demo-api-key": process.env.COINGECKO_API_KEY,
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const validCoins = response.data;
+    return validCoins.some(
+      (coin) => coin.name.toLowerCase() === coinName.toLowerCase()
+    );
+  } catch (error) {
+    console.error("Error fetching coin list:", error);
+    throw new Error("Failed to validate coin name");
+  }
+};
+
+module.exports = { validateCoinName };
