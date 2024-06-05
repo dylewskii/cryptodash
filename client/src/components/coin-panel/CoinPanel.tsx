cd467f04036248f2f3e64e062e2267d4d1f2064d
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-06-05 21:03:00 +0200

    pass coinId & coinName props to ConverterCard

--- client/src/components/coin-panel/CoinPanel.tsx
+++ client/src/components/coin-panel/CoinPanel.tsx
@@ -1,16 +1,17 @@
 import { useLocation } from "react-router-dom";
 import CoinInfoCard from "./CoinInfoCard";
 import CoinHoldingsCard from "./CoinHoldingsCard";
+import ConverterCard from "./ConverterCard";
 
 export default function CoinPanel() {
   const location = useLocation();
   const coinData = location.state.coin;
-  console.log(coinData);
 
   return (
     <div className="flex flex-col justify-between md:flex-row gap-3">
       <CoinHoldingsCard coin={coinData} />
       <CoinInfoCard coin={coinData} />
+      <ConverterCard coinId={coinData.id} coinName={coinData.name} />
     </div>
   );
 }
