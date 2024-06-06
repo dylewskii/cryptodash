fa24bc4037ab4b8fe8bd44fa94bdcf84ed60e667
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-06-06 20:27:54 +0200

    add maxFractionDigits parameter

--- client/src/lib/formatCurrency.ts
+++ client/src/lib/formatCurrency.ts
@@ -1,9 +1,13 @@
-const formatCurrency = (value: number, currency: string = "USD") => {
+const formatCurrency = (
+  value: number,
+  currency: string = "USD",
+  maxFractionDigits: number = 2
+) => {
   const currencyFormatter = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: currency,
     minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
+    maximumFractionDigits: maxFractionDigits,
   });
 
   return currencyFormatter.format(value);
