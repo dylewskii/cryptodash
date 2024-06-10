e6c0e53ee875aac21b4dff09539c06fcf8a9197a
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-06-10 23:34:45 +0200

    refactor func to return an object w/ all fiat currency values

--- client/src/hooks/useCryptoFiatValues.ts
+++ client/src/hooks/useCryptoFiatValues.ts
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
-// structure for caching the price of crypto w/ a timestamp
+// structure for caching the prices of crypto w/ a timestamp
 interface PriceCacheEntry {
-  value: number;
+  value: { [key: string]: number };
   timestamp: number;
 }
 
@@ -11,22 +11,21 @@ interface PriceCache {
   [key: string]: PriceCacheEntry;
 }
 
-export default function useCryptoFiatValue() {
+export default function useCryptoFiatValues() {
   const [priceCache, setPriceCache] = useState<PriceCache>({});
   /** -----------------------------------------------------------------------------------------------
-   * Fetches the current fiat value of the specified crypto coin.
+   * Fetches the current fiat values of the specified crypto coin.
    *
    * If the price has been fetched within the last hour and is cached, the cached value is used.
    * If the price is not cached or the cache is older than one hour, it fetches the price again.
    *
    * @param coinId string name of the crypto coin to fetch value for.
    * @param currency string name of the desired fiat currency to fetch the value of. Default = "usd"
-   * @returns A promise that resolves to the fiat value of the cryptocurrency.
+   * @returns A promise that resolves to an object containing the fiat values, in a range of currencies, of the specified crypto coin.
    */
-  const getCryptoFiatValue = async (
-    coinId: string,
-    currency = "usd"
-  ): Promise<number> => {
+  const getCryptoFiatValues = async (
+    coinId: string
+  ): Promise<{ [key: string]: number }> => {
     const currentTime = new Date().getTime();
     const cacheEntry = priceCache[coinId];
     const hourInMilliseconds = 3_600_000;
@@ -42,20 +41,20 @@ export default function useCryptoFiatValue() {
         `https://api.coingecko.com/api/v3/coins/${coinId}`
       );
       const data = await res.json();
-      const fiatValue = await data.market_data.current_price[currency];
+      const currentPriceData = await data.market_data.current_price;
 
       // update cache w/ new data
       setPriceCache((prev) => ({
         ...prev,
-        [coinId]: { value: fiatValue, timestamp: currentTime },
+        [coinId]: { value: currentPriceData, timestamp: currentTime },
       }));
 
-      return fiatValue;
+      return currentPriceData;
     } catch (error) {
       console.error("Failed to fetch crypto dollar value:", error);
-      return 0;
+      return {};
     }
   };
 
-  return { getCryptoFiatValue };
+  return { getCryptoFiatValues };
 }
