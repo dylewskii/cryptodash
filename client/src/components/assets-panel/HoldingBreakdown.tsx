5435585a7000eb17011bd39fd3ba63ee9da473e5
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-06-15 16:17:31 +0200

    add HoldingBreakdown

---
+++ client/src/components/assets-panel/HoldingBreakdown.tsx
@@ -0,0 +1,21 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+
+export default function HoldingBreakdown() {
+  return (
+    <>
+      <Card className="mt-4">
+        <CardHeader>
+          <CardTitle>Holding Breakdown</CardTitle>
+        </CardHeader>
+        <CardContent></CardContent>
+        <CardFooter></CardFooter>
+      </Card>
+    </>
+  );
+}
