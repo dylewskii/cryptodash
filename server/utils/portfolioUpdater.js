e51a09129a92b00eb32e68d25b10319818d1f5ff
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-06-21 23:08:02 +0200

    add cron schedule

--- server/utils/portfolioUpdater.js
+++ server/utils/portfolioUpdater.js
@@ -1,7 +1,18 @@
 const cron = require("node-cron");
+const User = require("../models/User");
+const {
+  fetchPortfolioValue,
+  addPortfolioValue,
+} = require("../services/portfolioServices");
 
-// cron job every hr
-cron.schedule("0 * * * *", () => {
-  console.log("portfolio value has been saved");
-  // update portfolio values function here
+cron.schedule("0 * * * *", async () => {
+  try {
+    const users = await User.find();
+    for (const user of users) {
+      const userPortfolioValue = await fetchPortfolioValue(user._id);
+      await addPortfolioValue(user._id, userPortfolioValue);
+    }
+  } catch (err) {
+    console.error("Error in portfolio value save cron job:", err);
+  }
 });
