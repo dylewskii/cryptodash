27754d7b92799742882f2b3bed5a3832c223e28c
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-06-25 12:24:11 +0300

    update filename

--- client/src/components/dashboard-panel/DashboardPanel.tsx
+++ client/src/components/dashboard-panel/DashboardPanel.tsx
@@ -1,13 +1,12 @@
 import PortfolioCard from "./PortfolioCard";
 import FavoriteCoins from "./FavoriteCoins";
 import TotalBalance from "./TotalBalance";
-import "./dashboard.css";
 
-export default function Dashboard() {
+export default function DashboardPanel() {
   return (
     <div className="mt-4 md:grid md:grid-cols-5 md:grid-rows-[auto_auto_auto] md:px-0 md:pr-8 max-w-screen-2xl">
       <section className="col-span-5 row-start-1">
-        <TotalBalance />
+        <TotalBalance className="mb-6" />
         <PortfolioCard />
       </section>
 
