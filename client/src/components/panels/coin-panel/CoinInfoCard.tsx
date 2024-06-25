4be2df4ee655d6173015120e15ad65d1da7d4656
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-06-25 21:28:58 +0300

    add ternary operators to prevent crashing

--- client/src/components/panels/coin-panel/CoinInfoCard.tsx
+++ client/src/components/panels/coin-panel/CoinInfoCard.tsx
@@ -25,35 +25,45 @@ export default function CoinInfoCard({ coin, className }: CoinInfoCardProps) {
         <div className="grid grid-cols-2 gap-y-4">
           <p className="font-semibold text-left">All-Time High:</p>
           <p className="font-normal text-right">
-            {formatCurrency(coin.info.ath)}
+            {coin.info.ath ? formatCurrency(coin.info.ath) : "N/A"}
           </p>
 
           <p className="font-semibold text-left">Market Cap Rank:</p>
-          <p className="font-normal text-right">{coin.info.market_cap_rank}</p>
+          <p className="font-normal text-right">
+            {coin.info.market_cap_rank ? coin.info.market_cap_rank : "N/A"}
+          </p>
 
           <p className="font-semibold text-left">Market Cap:</p>
           <p className="font-normal text-right">
-            {formatCurrency(coin.info.marketCap)}
+            {coin.info.marketCap ? formatCurrency(coin.info.marketCap) : "N/A"}
           </p>
 
           <p className="font-semibold text-left">Fully Diluted Value:</p>
           <p className="font-normal text-right">
-            {formatCurrency(Number(coin.info.fully_diluted_valuation))}
+            {coin.info.fully_diluted_valuation
+              ? formatCurrency(Number(coin.info.fully_diluted_valuation))
+              : "N/A"}
           </p>
 
           <p className="font-semibold text-left">Total Supply:</p>
           <p className="font-normal text-right">
-            {coin.info.total_supply.toLocaleString("en-US")}
+            {coin.info.total_supply
+              ? coin.info.total_supply.toLocaleString("en-US")
+              : "N/A"}
           </p>
 
           <p className="font-semibold text-left">Max Supply:</p>
           <p className="font-normal text-right">
-            {coin.info.max_supply.toLocaleString("en-US")}
+            {coin.info.max_supply
+              ? coin.info.max_supply.toLocaleString("en-US")
+              : "N/A"}
           </p>
 
           <p className="font-semibold text-left">Circulating Supply:</p>
           <p className="font-normal text-right">
-            {coin.info.circulating_supply.toLocaleString("en-US")}
+            {coin.info.circulating_supply
+              ? coin.info.circulating_supply.toLocaleString("en-US")
+              : "N/A"}
           </p>
 
           <p className="font-semibold text-left">Genesis Date:</p>
