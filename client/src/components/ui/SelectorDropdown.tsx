bfc273f60a8bd1cbe697617759ae2aa8a300a355
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-06-26 22:34:39 +0300

    add className prop

--- client/src/components/ui/SelectorDropdown.tsx
+++ client/src/components/ui/SelectorDropdown.tsx
@@ -20,6 +20,7 @@ interface Coin {
 }
 
 interface SelectorDropdownProps {
+  className?: string;
   label: string;
   items: Coin[];
   value: string;
@@ -27,6 +28,7 @@ interface SelectorDropdownProps {
 }
 
 export default function SelectorDropdown({
+  className,
   label,
   items,
   value,
@@ -40,66 +42,68 @@ export default function SelectorDropdown({
   );
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="mx-auto w-full"
-        >
-          {value || label}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command>
-          <div className="flex items-center border-b px-3">
-            <input
-              id="itemSelection"
-              type="text"
-              placeholder={`${label}...`}
-              value={filter}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setFilter(e.target.value)
-              }
-              className="flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50"
-            />
-          </div>
-          <CommandGroup>
-            <CommandList>
-              <List
-                height={200}
-                itemCount={filteredItems.length}
-                itemSize={35}
-                width="100%"
-              >
-                {({ index, style }) => {
-                  const item = filteredItems[index];
-                  return (
-                    <CommandItem
-                      key={item.id}
-                      value={item.name}
-                      style={style}
-                      onSelect={() => {
-                        onChange(item.name === value ? "" : item.name);
-                        setOpen(false);
-                      }}
-                    >
-                      <Check
-                        className={`mr-2 h-4 w-4 ${
-                          item.name === value ? "opacity-100" : "opacity-0"
-                        }`}
-                      />
-                      {item.name}
-                    </CommandItem>
-                  );
-                }}
-              </List>
-            </CommandList>
-          </CommandGroup>
-        </Command>
-      </PopoverContent>
-    </Popover>
+    <div className={`${className}`}>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="mx-auto w-full"
+          >
+            {value || label}
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <div className="flex items-center border-b px-3">
+              <input
+                id="itemSelection"
+                type="text"
+                placeholder={`${label}...`}
+                value={filter}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setFilter(e.target.value)
+                }
+                className="flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50"
+              />
+            </div>
+            <CommandGroup>
+              <CommandList>
+                <List
+                  height={200}
+                  itemCount={filteredItems.length}
+                  itemSize={35}
+                  width="100%"
+                >
+                  {({ index, style }) => {
+                    const item = filteredItems[index];
+                    return (
+                      <CommandItem
+                        key={item.id}
+                        value={item.name}
+                        style={style}
+                        onSelect={() => {
+                          onChange(item.name === value ? "" : item.name);
+                          setOpen(false);
+                        }}
+                      >
+                        <Check
+                          className={`mr-2 h-4 w-4 ${
+                            item.name === value ? "opacity-100" : "opacity-0"
+                          }`}
+                        />
+                        {item.name}
+                      </CommandItem>
+                    );
+                  }}
+                </List>
+              </CommandList>
+            </CommandGroup>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </div>
   );
 }
