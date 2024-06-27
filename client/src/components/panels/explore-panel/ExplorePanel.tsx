c59b1303dc274390e23a82c2dd3753abd5dab1bc
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-06-27 13:34:47 +0300

    add isFocusVisible optional class

--- client/src/components/panels/explore-panel/ExplorePanel.tsx
+++ client/src/components/panels/explore-panel/ExplorePanel.tsx
@@ -64,7 +64,7 @@ export default function ExplorePanel() {
         </div>
 
         <Input
-          className="border-none focus-visible:outline-none"
+          className="border-none"
           placeholder="Search the cryptocurrency market"
           type="text"
           value={query}
