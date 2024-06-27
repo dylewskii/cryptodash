4ed279d94a945d0941b1db8350cce46f22b21f6a
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-06-28 00:54:15 +0300

    render SparkLineChart within each coin card

--- client/src/components/panels/dashboard-panel/FavoriteCoins.tsx
+++ client/src/components/panels/dashboard-panel/FavoriteCoins.tsx
@@ -6,12 +6,14 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Skeleton } from "../../ui/skeleton";
 // utils
 import formatCurrency from "@/lib/formatCurrency";
+import SparkLineChart from "@/charts/SparkLineChart";
 
 export default function FavoriteCoins() {
   const { portfolio, loading } = useContext(UserContext);
@@ -28,40 +30,48 @@ export default function FavoriteCoins() {
           </Card>
         </>
       ) : (
-        portfolio.detailed.map((coinObject, i) => {
+        portfolio.detailed.map((portfolioCoin, i) => {
           if (
-            !coinObject ||
-            !coinObject.info ||
-            coinObject.info.currentPrice === undefined
+            !portfolioCoin ||
+            !portfolioCoin.info ||
+            portfolioCoin.info.currentPrice === undefined
           ) {
             return (
-              <Card key={`${coinObject.name}${i}`}>
+              <Card key={`${portfolioCoin.name}${i}`}>
                 <Skeleton className="h-[100px] w-full rounded-xl" />
               </Card>
             );
           }
 
-          const coinPrice = coinObject.info.currentPrice;
+          const coinPrice = portfolioCoin.info.currentPrice;
           let underTwoDecimals = false;
           if (coinPrice < 0.01) {
             underTwoDecimals = true;
           }
 
           return (
-            <Card key={`${coinObject.name}${i}`}>
+            <Card key={`${portfolioCoin.name}${i}`}>
               <CardHeader>
-                <CardTitle>{coinObject.name}</CardTitle>
-                <CardDescription>
-                  {coinObject.info.symbol.toUpperCase()}
+                <CardTitle className="z-50">{portfolioCoin.name}</CardTitle>
+                <CardDescription className="z-50">
+                  {portfolioCoin.info.symbol.toUpperCase()}
                 </CardDescription>
               </CardHeader>
-              <CardContent>
-                <p className="text-3xl">
+              <CardContent className="py-0 px-6">
+                <p className="text-black text-3xl z-50">
                   {underTwoDecimals
                     ? formatCurrency(coinPrice, "USD", 6)
                     : formatCurrency(coinPrice, "USD", 2)}
                 </p>
               </CardContent>
+              <CardFooter className="flex justify-center p-0 overflow-hidden">
+                <SparkLineChart
+                  prices={portfolioCoin.info.sparkline}
+                  width="270px"
+                  height="100px"
+                  className="bottom-0 z-0 pointer-events-none"
+                />
+              </CardFooter>
             </Card>
           );
         })
