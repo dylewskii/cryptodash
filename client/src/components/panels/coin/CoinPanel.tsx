c19c1d1b0f33aa74894f9fdfa5c913790861e086
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-07-09 00:31:46 +0300

    listen out for portfolio changes using socket.io

--- client/src/components/panels/coin/CoinPanel.tsx
+++ client/src/components/panels/coin/CoinPanel.tsx
@@ -1,19 +1,54 @@
+// react
 import { useLocation } from "react-router-dom";
-import { useState } from "react";
-import { DetailedCoin } from "@/types";
+import { useEffect, useState } from "react";
+// ui
 import TokenInfoCard from "./TokenInfoCard";
 import HoldingsCard from "./HoldingsCard";
 import ConverterCard from "./ConverterCard";
 import CoinPriceCard from "./CoinPriceCard";
+// store
+import { useUserStore } from "@/stores/useUserStore";
+// socket
+import socket from "@/socket/socket";
+/// types
+import { CoinDB } from "@/types";
+import { DetailedCoin } from "@/types";
+
+interface PortfolioUpdateEvent {
+  userId: string;
+  portfolio: CoinDB[];
+}
 
 export default function CoinPanel() {
   const location = useLocation();
   const initialCoinData = location.state.coin as DetailedCoin;
   const [coinData, setCoinData] = useState<DetailedCoin>(initialCoinData);
+  const user = useUserStore((state) => state.user);
 
-  const updateCoinData = (updatedCoin: DetailedCoin) => {
-    setCoinData(updatedCoin);
-  };
+  useEffect(() => {
+    const handlePortfolioUpdate = ({
+      userId,
+      portfolio,
+    }: PortfolioUpdateEvent) => {
+      if (user.userId === userId) {
+        // find the updated coin data in the new portfolio
+        const updatedCoin = portfolio.find((coin) => coin.id === coinData.id);
+        if (updatedCoin) {
+          setCoinData((prevCoin) => ({
+            ...prevCoin,
+            amount: updatedCoin.amount.toString(), // keeps same type (string)
+            totalValue: updatedCoin.amount * prevCoin.info.currentPrice,
+          }));
+        }
+      }
+    };
+
+    socket.on("portfolioUpdated", handlePortfolioUpdate);
+
+    return () => {
+      socket.off("portfolioUpdated", handlePortfolioUpdate);
+    };
+  }, [user.userId, coinData.id]);
 
   return (
     <section className="grid gap-4 mb-6 grid-cols-1 lg:grid-cols-2">
@@ -22,7 +57,6 @@ export default function CoinPanel() {
 
         <HoldingsCard
           coin={coinData}
-          updateCoinData={updateCoinData}
           className="w-full row-start-2 mt-6 lg:row-start-2 mt-4"
         />
       </div>
