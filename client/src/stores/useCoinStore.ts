b23b1f48f1e116ee9baf06cf6b042eb2866ebbd2
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-07-09 15:59:57 +0300

    change fetchAllCoins to fetchCoinsByPage

--- client/src/stores/useCoinStore.ts
+++ client/src/stores/useCoinStore.ts
@@ -5,26 +5,24 @@ import { useUserStore } from "./useUserStore";
 interface CoinStore {
   cryptoList: MarketDataCoin[];
   setCryptoList: (cryptoList: MarketDataCoin[]) => void;
-  fetchAllCoinsPending: boolean;
-  setFetchAllCoinsPending: (fetchAllCoinsPending: boolean) => void;
-  fetchAllCoins: (page: number) => Promise<void>;
+  // --------------------------------------------------------- //
+  fetchCoinsByPage: (page: number) => Promise<void>;
+  fetchCoinsByPagePending: boolean;
+  setFetchCoinsByPagePending: (fetchCoinsByPagePending: boolean) => void;
 }
 
 export const useCoinStore = create<CoinStore>((set) => ({
   cryptoList: [],
   setCryptoList: (cryptoList) => set({ cryptoList }),
-  fetchAllCoinsPending: false,
-  setFetchAllCoinsPending: (fetchAllCoinsPending) =>
-    set({ fetchAllCoinsPending }),
-
-  fetchAllCoins: async (page: number) => {
+  // --------------------------------------------------------- //
+  fetchCoinsByPage: async (page: number) => {
     const { user, accessToken } = useUserStore.getState();
     if (!user.isAuthenticated) {
-      set({ fetchAllCoinsPending: false });
+      set({ fetchCoinsByPagePending: false });
       return;
     }
 
-    set({ fetchAllCoinsPending: true });
+    set({ fetchCoinsByPagePending: true });
 
     try {
       const url = `http://localhost:8000/data/all-coins-with-market-data?page=${page}`;
@@ -37,21 +35,34 @@ export const useCoinStore = create<CoinStore>((set) => ({
       };
 
       const response = await fetch(url, options);
+
+      if (!response.ok) {
+        set({ fetchCoinsByPagePending: false });
+        throw new Error(`Http error: ${response.status}`);
+      }
+
       const { data } = await response.json();
 
-      const coins = data.map((coin: MarketDataCoin) => ({
-        ...coin,
-        sparkline_in_7d: coin.sparkline_in_7d.price,
-      }));
+      // const coins = data.map((coin: MarketDataCoin) => ({
+      //   ...coin,
+      //   sparkline_in_7d: coin.sparkline_in_7d.price,
+      // }));
+
+      const coinsFromPage = data;
 
       set((state) => ({
-        cryptoList: page === 1 ? coins : [...state.cryptoList, ...coins],
+        cryptoList:
+          page === 1 ? coinsFromPage : [...state.cryptoList, ...coinsFromPage],
       }));
     } catch (error) {
       console.error("Error fetching coins:", error);
       // error state ?
     } finally {
-      set({ fetchAllCoinsPending: false });
+      set({ fetchCoinsByPagePending: false });
     }
   },
+  // --------------------------------------------------------- //
+  fetchCoinsByPagePending: false,
+  setFetchCoinsByPagePending: (fetchCoinsByPagePending) =>
+    set({ fetchCoinsByPagePending }),
 }));
