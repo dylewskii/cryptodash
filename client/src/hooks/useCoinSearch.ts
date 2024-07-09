a9247b9b50f53a4d2caa518a259f138ba52f62a4
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-07-09 23:27:02 +0300

    add useCoinSearch hook

---
+++ client/src/hooks/useCoinSearch.ts
@@ -0,0 +1,79 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// react
+import { useState, useEffect } from "react";
+// stores
+import { useCoinStore } from "@/stores/useCoinStore";
+import { useUserStore } from "@/stores/useUserStore";
+// utils
+import { useDebounce } from "use-debounce";
+
+export const useCoinSearch = (searchTerm: string) => {
+  const [searchResults, setSearchResults] = useState<any[]>([]);
+  const [isSearchLoading, setIsSearchLoading] = useState(false);
+  const [debouncedSearchTerm] = useDebounce(searchTerm, 300);
+  const accessToken = useUserStore((state) => state.accessToken);
+  const cryptoList = useCoinStore((state) => state.cryptoList);
+
+  // initally sets the list to the first 100 coins
+  useEffect(() => {
+    if (cryptoList.length > 0 && !debouncedSearchTerm) {
+      const initialCoins = cryptoList.slice(0, 100).map((coin) => ({
+        id: coin.id,
+        name: coin.name,
+      }));
+      setSearchResults(initialCoins);
+    }
+  }, [cryptoList, debouncedSearchTerm]);
+
+  //
+  useEffect(() => {
+    const runSearch = async () => {
+      if (!accessToken) {
+        return;
+      }
+
+      // if search empty, revert to initial 100 coins
+      if (!debouncedSearchTerm) {
+        const initialCoins = cryptoList.slice(0, 100).map((coin) => ({
+          id: coin.id,
+          name: coin.name,
+        }));
+        setSearchResults(initialCoins);
+        return;
+      }
+
+      const url = `http://localhost:8000/data/search?searchTerm=${debouncedSearchTerm}`;
+      const options: RequestInit = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+      try {
+        if (debouncedSearchTerm) {
+          setIsSearchLoading(true);
+          const response = await fetch(url, options);
+
+          if (!response.ok) {
+            throw new Error(`Http error: ${response.status}`);
+          }
+
+          const data = await response.json();
+
+          setSearchResults(data.data.coins);
+        } else {
+          setSearchResults([]);
+        }
+      } catch (err) {
+        throw new Error(`Failed to debounce search term ${err}`);
+      } finally {
+        setIsSearchLoading(false);
+      }
+    };
+
+    runSearch();
+  }, [debouncedSearchTerm, accessToken, cryptoList]);
+
+  return { searchResults, isSearchLoading };
+};
