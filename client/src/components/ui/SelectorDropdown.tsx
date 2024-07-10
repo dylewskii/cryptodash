023413e5fb2d01aa117218ccbd8efaa9131319cf
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-07-10 19:30:26 +0300

    redesign component to handle coin ids instead of names

--- client/src/components/ui/SelectorDropdown.tsx
+++ client/src/components/ui/SelectorDropdown.tsx
@@ -1,6 +1,8 @@
+// react
 import { useState } from "react";
-import { Check, ChevronsUpDown } from "lucide-react";
 import { FixedSizeList as List } from "react-window";
+// ui
+import { Check, ChevronsUpDown } from "lucide-react";
 import { Button } from "../ui/button";
 import {
   Popover,
@@ -13,89 +15,96 @@ import {
   CommandItem,
   CommandList,
 } from "@/components/ui/command";
-
-interface Coin {
-  id: string;
-  name: string;
-}
+// hook & type
+import { useCoinSearch, AddedCoin } from "@/hooks/useCoinSearch";
 
 interface SelectorDropdownProps {
   className?: string;
   label: string;
-  items: Coin[];
-  value: string;
-  onChange: (value: string) => void;
+  value: string | null;
+  onChange: (value: AddedCoin) => void;
 }
 
 export default function SelectorDropdown({
   className,
   label,
-  items,
   value,
   onChange,
 }: SelectorDropdownProps) {
   const [open, setOpen] = useState(false);
-  const [filter, setFilter] = useState("");
+  const [query, setQuery] = useState("");
+  const { searchResults } = useCoinSearch(query);
 
-  const filteredItems = items.filter((item) =>
-    item.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const selectedCoin = searchResults.find((coin) => coin.id === value);
 
   return (
     <div className={`${className}`}>
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
+          {/* ---- SELECTOR BUTTON WITH LABEL OR COIN NAME ----*/}
           <Button
             variant="outline"
             role="combobox"
             aria-expanded={open}
             className="mx-auto w-full"
           >
-            {value || label}
+            {selectedCoin ? selectedCoin.name : label}
             <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0">
+        <PopoverContent className="w-[275px] p-0">
           <Command>
             <div className="flex items-center border-b px-3">
+              {/* ---- SEARCHBOX ----*/}
               <input
                 id="itemSelection"
                 type="text"
                 placeholder={`${label}...`}
-                value={filter}
+                value={query}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setFilter(e.target.value)
+                  setQuery(e.target.value)
                 }
                 className="flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50"
               />
             </div>
             <CommandGroup>
+              {/* ---- COIN LIST ----*/}
               <CommandList>
                 <List
                   height={200}
-                  itemCount={filteredItems.length}
+                  itemCount={searchResults.length}
                   itemSize={35}
                   width="100%"
                 >
                   {({ index, style }) => {
-                    const item = filteredItems[index];
+                    const item = searchResults[index];
                     return (
-                      <CommandItem
-                        key={item.id}
-                        value={item.name}
-                        style={style}
-                        onSelect={() => {
-                          onChange(item.name === value ? "" : item.name);
-                          setOpen(false);
-                        }}
-                      >
-                        <Check
-                          className={`mr-2 h-4 w-4 ${
-                            item.name === value ? "opacity-100" : "opacity-0"
-                          }`}
-                        />
-                        {item.name}
-                      </CommandItem>
+                      <>
+                        <CommandItem
+                          className=""
+                          key={item.id}
+                          value={item.id}
+                          style={style}
+                          onSelect={() => {
+                            onChange(item);
+                            setOpen(false);
+                          }}
+                        >
+                          <Check
+                            className={`mr-2 h-4 w-4 ${
+                              item.id === value ? "opacity-100" : "opacity-0"
+                            }`}
+                          />
+                          {item.thumb ? (
+                            <img
+                              src={`${item.thumb}`}
+                              alt="Cryptocurrency Logo"
+                              className="z-10 max-w-[25px] pr-2"
+                            />
+                          ) : null}
+                          {item.name}
+                        </CommandItem>
+                      </>
                     );
                   }}
                 </List>
