6da453d8c062e39ce42bec80f9e39671a8246a5b
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-07-11 19:38:57 +0300

    add genRandomHexColor

--- client/src/lib/stringUtils.ts
+++ client/src/lib/stringUtils.ts
@@ -1,3 +1,7 @@
 export const capitalizeFirstLetter = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
+
+export const genRandomHexColor = () => {
+  return "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
+};
