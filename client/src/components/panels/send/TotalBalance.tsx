07e09e6823f4c0f49bb8ec4b1097cec6fdbed78f
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-07-11 22:07:54 +0300

    install postcss

--- client/src/components/panels/send/TotalBalance.tsx
+++
@@ -1,9 +0,0 @@
-export default function TotalBalance() {
-  return (
-    <div className="flex justify-center text-black py-4">
-      <p className="text-4xl">
-        <span>$</span>100,000
-      </p>
-    </div>
-  );
-}
