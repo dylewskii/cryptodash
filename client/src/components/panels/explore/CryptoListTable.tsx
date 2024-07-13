dd00e489848ab8e99fa3e2fe36ed167bc01d6140
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-07-13 21:10:50 +0300

    fix types & improve loading ux

--- client/src/components/panels/explore/CryptoListTable.tsx
+++ client/src/components/panels/explore/CryptoListTable.tsx
@@ -11,6 +11,7 @@ import { Skeleton } from "../../ui/skeleton";
 // utils
 import { formatCurrency, roundToTwoDecimalPlaces } from "@/lib";
 import { useNavigate } from "react-router-dom";
+import { AddedCoin } from "@/hooks/useCoinSearch";
 
 export interface CoinObject {
   ath: number;
@@ -42,7 +43,7 @@ export interface CoinObject {
 }
 
 interface CryptoListTableProps {
-  cryptoList: CoinObject[];
+  cryptoList: (CoinObject | AddedCoin)[];
   loading: boolean;
 }
 
@@ -68,78 +69,94 @@ export default function CryptoListTable({
         </TableRow>
       </TableHeader>
       <TableBody>
-        {cryptoList.map((coinObject: CoinObject, i) => (
-          <TableRow
-            key={`${coinObject.name}${i}`}
-            className="cursor-pointer"
-            onClick={() => handleRowClick(coinObject.id)}
-          >
-            <TableCell>
-              <div className="font-medium flex items-center">
-                {coinObject.market_cap_rank}
-              </div>
-            </TableCell>
-            <TableCell>
-              <img src={coinObject.image} alt="" />
-            </TableCell>
-            <TableCell>{coinObject.name}</TableCell>
-            <TableCell>{formatCurrency(coinObject.current_price)}</TableCell>
-            <TableCell
-              className={
-                coinObject.price_change_percentage_24h < 0
-                  ? "text-red-600"
-                  : "text-green-600"
-              }
+        {cryptoList.map((coin, i) => {
+          const isCoinObject = "current_price" in coin;
+          return (
+            <TableRow
+              key={`${coin.name}${i}`}
+              className="cursor-pointer"
+              onClick={() => handleRowClick(coin.id)}
             >
-              <div className="flex justify-end items-center">
-                {roundToTwoDecimalPlaces(
-                  coinObject.price_change_percentage_24h
-                ) + "%"}
-                {coinObject.price_change_percentage_24h < 0 ? (
-                  <svg
-                    className="w-4 h-4 mt-[2px]"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M6 9L12 15L18 9"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="w-4 h-4 mt-[2px]"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M18 15L12 9L6 15"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                )}
-              </div>
-            </TableCell>
-            <TableCell className="text-right">
-              {formatCurrency(coinObject.market_cap, "USD", 2, 0)}
-            </TableCell>
-          </TableRow>
-        ))}
-        {loading && (
-          <TableRow>
-            <TableCell colSpan={6} className="text-center">
-              <Skeleton className="w-full h-10" />
-            </TableCell>
-          </TableRow>
-        )}
+              <TableCell>
+                <div className="font-medium flex items-center">
+                  {isCoinObject
+                    ? coin.market_cap_rank
+                    : coin.market_cap_rank || "N/A"}
+                </div>
+              </TableCell>
+              <TableCell>
+                <img
+                  src={isCoinObject ? coin.image : coin.thumb}
+                  alt={`${coin.name} logo`}
+                />
+              </TableCell>
+              <TableCell>{coin.name}</TableCell>
+              <TableCell>
+                {isCoinObject ? formatCurrency(coin.current_price) : "N/A"}
+              </TableCell>
+              <TableCell
+                className={
+                  isCoinObject && coin.price_change_percentage_24h < 0
+                    ? "text-red-600"
+                    : "text-green-600"
+                }
+              >
+                <div className="flex justify-end items-center">
+                  {isCoinObject
+                    ? `${roundToTwoDecimalPlaces(
+                        coin.price_change_percentage_24h
+                      )}%`
+                    : "N/A"}
+                  {isCoinObject &&
+                    (coin.price_change_percentage_24h < 0 ? (
+                      <svg
+                        className="w-4 h-4 mt-[2px]"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M6 9L12 15L18 9"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        className="w-4 h-4 mt-[2px]"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M18 15L12 9L6 15"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                    ))}
+                </div>
+              </TableCell>
+              <TableCell className="text-right">
+                {isCoinObject
+                  ? formatCurrency(coin.market_cap, "USD", 2, 0)
+                  : "N/A"}
+              </TableCell>
+            </TableRow>
+          );
+        })}
+        {loading &&
+          Array.from({ length: 8 }).map((_, index) => (
+            <TableRow key={index}>
+              <TableCell colSpan={6} className="text-center">
+                <Skeleton className="w-full h-12" />
+              </TableCell>
+            </TableRow>
+          ))}
       </TableBody>
     </Table>
   );
