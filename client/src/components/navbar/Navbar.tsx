03b2f60014607caf4e3339197033401c8f1fdaa5
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-07-14 20:24:39 +0300

    fix logout

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -13,30 +13,40 @@ import { useUserStore } from "@/stores/useUserStore";
 export default function Navbar() {
   const user = useUserStore((state) => state.user);
   const setUser = useUserStore((state) => state.setUser);
+  const setAccessToken = useUserStore((state) => state.setAccessToken);
+  const accessToken = useUserStore((state) => state.accessToken);
   const profilePicUrl = useUserStore((state) => state.profilePicUrl);
   const navigate = useNavigate();
 
   const logoutUser = async () => {
-    // invalidate cookie server-side
-    const response = await fetch("http://localhost:8000/auth/logout", {
-      credentials: "include",
-    });
+    try {
+      // invalidate cookie server-side
+      const response = await fetch("http://localhost:8000/auth/logout", {
+        credentials: "include",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
 
-    if (!response.ok) {
-      console.error("Unable to logout");
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+
+      // reset access token & user state
+      setAccessToken("");
+      setUser({
+        userId: "",
+        username: "",
+        isAuthenticated: false,
+        email: "",
+      });
+
+      // redirect
+      navigate("/login");
+      return;
+    } catch (err) {
+      console.error(err);
     }
-
-    // reset user state
-    setUser({
-      userId: "",
-      username: "",
-      isAuthenticated: false,
-      email: "",
-    });
-
-    // redirect
-    navigate("/login");
-    return;
   };
 
   return (
@@ -48,22 +58,6 @@ export default function Navbar() {
         </p>
       </div>
       <nav className="flex gap-4 items-center">
-        <div className="controls">
-          <svg
-            className="w-6 h-6 cursor-pointer"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M14 21H10M18 8C18 6.4087 17.3679 4.88258 16.2427 3.75736C15.1174 2.63214 13.5913 2 12 2C10.4087 2 8.8826 2.63214 7.75738 3.75736C6.63216 4.88258 6.00002 6.4087 6.00002 8C6.00002 11.0902 5.22049 13.206 4.34968 14.6054C3.61515 15.7859 3.24788 16.3761 3.26134 16.5408C3.27626 16.7231 3.31488 16.7926 3.46179 16.9016C3.59448 17 4.19261 17 5.38887 17H18.6112C19.8074 17 20.4056 17 20.5382 16.9016C20.6852 16.7926 20.7238 16.7231 20.7387 16.5408C20.7522 16.3761 20.3849 15.7859 19.6504 14.6054C18.7795 13.206 18 11.0902 18 8Z"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </div>
         <div className="profile">
           <DropdownMenu>
             <DropdownMenuTrigger>
